64ee8304f8b9afe98e2a796e115a05dee41602b6
AntVill77 <antvillot77@hotmail.com>
2022-04-05 13:43:44 -0500

    actualizando codigo

---
+++ app/src/main/java/org/example/myapplication_por_default/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.example.myapplication_por_default
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
